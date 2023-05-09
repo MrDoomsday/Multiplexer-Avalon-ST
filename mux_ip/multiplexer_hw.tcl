da45b81554045cb46785dfe7ddbdc42da0412083
RB <belenkov_rn@kursksu.ru>
2023-05-09 20:49:51 +0300

    TCL_edit
    Convert CRLF->LF;
    Remove HDL visible in TCL script

--- mux_ip/multiplexer_hw.tcl
+++ mux_ip/multiplexer_hw.tcl
@@ -81,7 +81,7 @@ set_parameter_property channel_width_gui DISPLAY_NAME "Width channel"
 set_parameter_property channel_width_gui TYPE INTEGER
 set_parameter_property channel_width_gui ALLOWED_RANGES {0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 25 26 27 28 29 30 31 32}
 set_parameter_property channel_width_gui UNITS None
-set_parameter_property channel_width_gui HDL_PARAMETER true
+set_parameter_property channel_width_gui HDL_PARAMETER false
 
 
 
